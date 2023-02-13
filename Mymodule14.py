3cc7dde76c5e96751a1a6469aa228ad3596d5fea
Suren <115506927+SurenSarkisyan@users.noreply.github.com>
2023-02-13 16:57:17 +0300

    Add files via upload

---
+++ Mymodule14.py
@@ -0,0 +1,10 @@
+name = "Bob"
+
+def hello():
+    print("Hello", name)
+
+def add_three_numbers(a, b, z):
+    if a != 0 and b != 0 and z != 0:
+        return a + b + z
+    else:
+        return "Some is zero"
